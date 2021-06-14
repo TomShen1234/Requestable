4dcd9cdb1682f135208d7ab9d2c7df9afadef1ed
Tom Shen <tomshen2004@163.com>
2021-06-14 12:22:11 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Tom Shen on 2021/6/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Binding var document: RequestableDocument
+
+    var body: some View {
+        TextEditor(text: $document.text)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(document: .constant(RequestableDocument()))
+    }
+}
---
+++ Shared/RequestableApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestableApp.swift
+//  Shared
+//
+//  Created by Tom Shen on 2021/6/14.
+//
+
+import SwiftUI
+
+@main
+struct RequestableApp: App {
+    var body: some Scene {
+        DocumentGroup(newDocument: RequestableDocument()) { file in
+            ContentView(document: file.$document)
+        }
+    }
+}
